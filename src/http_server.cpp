9ac5b3741e8e11dfc187c8694d8126ac5bbdd9e3
qianlv <qianlv7@qq.com>
2024-08-13 22:34:06 +0800

    rm http_server.cpp and time out config

--- src/http_server.cpp
+++
@@ -1,5 +0,0 @@
-#include "http_server.hpp"
-
-namespace co_io {
-
-} // namespace co_io
--- src/http_server.hpp
+++ src/http_server.hpp
@@ -10,7 +10,8 @@ namespace co_io {
 
 template <typename LoopType> class HttpServer {
 public:
-  HttpServer(std::string_view ip, std::string port);
+  HttpServer(std::string_view ip, std::string port,
+             unsigned int time_out_sec = 0);
 
   void start() {
     accept();
@@ -20,14 +21,16 @@ public:
 private:
   std::unique_ptr<LoopBase> loop_;
   AsyncFile listener_;
+  unsigned int time_out_sec_ = 0;
 
   TaskNoSuspend<void> accept();
   TaskNoSuspend<void> client(int fd);
 };
 
 template <typename LoopType>
-HttpServer<LoopType>::HttpServer(std::string_view ip, std::string port)
-    : loop_(std::make_unique<LoopType>()) {
+HttpServer<LoopType>::HttpServer(std::string_view ip, std::string port,
+                                 unsigned int time_out_sec)
+    : loop_(std::make_unique<LoopType>()), time_out_sec_(time_out_sec) {
   AddressSolver solver{ip, port};
   AddressSolver::AddressInfo info = solver.get_address_info();
   listener_ = AsyncFile::bind(info, loop_.get());
@@ -38,14 +41,13 @@ TaskNoSuspend<void> HttpServer<LoopType>::accept() {
   while (true) {
     AddressSolver::Address addr;
     int fd = co_await listener_.async_accept(addr);
-    // std::cerr << "new connecton = " << fd << std::endl;
     client(fd);
   }
 }
 
 template <typename LoopType>
 TaskNoSuspend<void> HttpServer<LoopType>::client(int fd) {
-  HttpConnection conn(AsyncFile{fd, loop_.get(), 0});
+  HttpConnection conn(AsyncFile{fd, loop_.get(), time_out_sec_});
   co_await conn.handle();
   co_return;
 }
