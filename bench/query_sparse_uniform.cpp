9055c66e1d741a530d83ec59672fe348f63bc1c3
qianlv <qianlv7@qq.com>
2024-08-18 15:07:44 +0800

    add bench for ada radix tree

---
+++ bench/query_sparse_uniform.cpp
@@ -0,0 +1,70 @@
+#include <benchmark/benchmark.h>
+
+#include "adaptive_radix_tree.hpp"
+#include <map>
+#include <random>
+#include <unordered_map>
+
+static void BM_AdaptiveRadixTree(benchmark::State &state) {
+  AdaptiveRadixTree<int> tree;
+  std::hash<uint64_t> hasher;
+  std::mt19937 rng(0);
+  int count = 0;
+  for (auto _ : state) {
+    tree.insert(std::to_string(hasher(rng())), count);
+    count += 1;
+  }
+
+  int result;
+  std::mt19937 rng2(0);
+  for (auto _ : state) {
+    tree.search(std::to_string(hasher(rng2())), result);
+  }
+}
+
+BENCHMARK(BM_AdaptiveRadixTree);
+
+static void BM_unordered_map(benchmark::State &state) {
+  std::unordered_map<std::string, int> tree;
+  std::hash<uint64_t> hasher;
+  std::mt19937 rng(0);
+  int count = 0;
+  for (auto _ : state) {
+    // tree.insert_or_assign(std::to_string(hasher(rng())), count);
+    tree[std::to_string(hasher(rng()))] = count;
+    count += 1;
+  }
+
+  int result;
+  (void)result;
+  std::mt19937 rng2(0);
+  for (auto _ : state) {
+    // tree.find(std::to_string(hasher(rng2())));
+    result = tree[std::to_string(hasher(rng2()))];
+  }
+}
+
+BENCHMARK(BM_unordered_map);
+
+static void BM_map(benchmark::State &state) {
+  std::map<std::string, int> tree;
+  std::hash<uint64_t> hasher;
+  std::mt19937 rng(0);
+  int count = 0;
+  for (auto _ : state) {
+    // tree.insert_or_assign(std::to_string(hasher(rng())), count);
+    tree[std::to_string(hasher(rng()))] = count;
+    count += 1;
+  }
+
+  int result = 0;
+  (void)result;
+  std::mt19937 rng2(0);
+  for (auto _ : state) {
+    // tree.at(std::to_string(hasher(rng2())));
+    result = tree[std::to_string(hasher(rng2()))];
+  }
+}
+
+BENCHMARK(BM_map);
+BENCHMARK_MAIN();
--- src/http_router.hpp
+++ src/http_router.hpp
@@ -4,7 +4,6 @@
 #include "http_util.hpp"
 #include <functional>
 #include <string>
-#include <unordered_map>
 
 namespace co_io {
 
